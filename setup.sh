8769e052131d76f042eb924f04da044a12f48dd7
Daisuke Miura <mhden@drakontia.com>
2017-04-14 14:46:54 +0900

    Download git-prompt.sh
    Because, use __git_ps1 function.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget -O ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
 DOT_FILES=( .bashrc .ctags .dir_colors .gdbinit .gemrc .gitconfig .gitignore .inputrc .minttyrc .profile .simplenote.rc .rdebugrc .vimrc)
 
 for file in ${DOT_FILES[@]}
